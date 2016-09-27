e48bd2a138056c4127c5a2484bf89d748b5059b2
Uday Gorrepati <uday.gorrepati@gmail.com>
2016-09-26 19:43:39 -0500

    Create initial html tests (some <body> text, <title>, and limited sementic element checks.

--- frb/spec/features/hw_1_spec.rb
+++ frb/spec/features/hw_1_spec.rb
@@ -3,27 +3,50 @@ require "rails_helper"
 show_tests_in_browser = true
 do_not_show_tests_in_browser = false
 
-feature "Homework 1:", js: do_not_show_tests_in_browser do
+feature "HTML page:", js: do_not_show_tests_in_browser do
+  before :each do
+    visit "/first-assignment.html"
+  end
 
-  scenario "user authentication required for likes page", points: 1 do
-    visit "/404.html"
+  # IGNORE BECAUSE NEXT TEST DOESN'T ALWAYS WORK
+  # scenario "page is not empty", points: 1 do
+  #   expect(page).to have_content
+  # end
 
-    expect(page).to have_link(nil, href: "/photos/new")
+  # SOMETIMES DOES NOT WORK BECAUSE ELEMENTS SEEM TO BE FILLED IN AUTOMATICALLY IF A FILE ONLY CONTAINS TEXT OR CERTAIN ELEMENTS
+  # scenario "contains some basic html structure", points: 1 do
+  #   expect(page).to have_selector("html")
+  #   expect(page).to have_selector("body")
+  # end
+
+  scenario "<body> element has text (hopefully some Raw Content added)", points: 1 do
+    expect(page.find("body").text).not_to eq("")
+  end
+
+  scenario "has a title", points: 1 do
+    expect(page.title).not_to eq nil
+    expect(page.title).not_to eq ""
+  end
+
+  scenario "includes some semantic links (you should have more, but this test only looks for a link, paragraph, and list item)", points: 3 do
+    expect(page).to have_selector("a")
+    expect(page).to have_selector("p")
+    expect(page).to have_selector("li")
+  end
+
+
+end
+
+
+
     # expect(page).to have_selector("form", count: 1)
     # expect(page).to have_selector("label", text: "Email")
-    # expect(page).to have_selector("label", text: "Password")
     # expect(page).to have_selector("input[type=submit][value='Log in']")
-    # expect(page).not_to have_selector("h1", text: "Likes")
-    # expect(page).not_to have_link("New Like")
     # expect(page).to have_content("Signed in successfully.")
     # within("nav") {
     #   expect(page).to have_link(user.username, href: "/users/edit")
     #   expect(page).to have_link(nil, href: "/users/sign_out")
     #   expect(page).not_to have_link("Dummy Sign Out Link")
     # }
-  end
-
-
-end
 
 
